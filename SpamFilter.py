8a8418f5e8bbab66b7fb0b260b512f4258561ae5
Eshaan Shah <seshaan.259@gmail.com>
2017-10-29 23:29:26 -0400

    Update SpamFilter.py

--- SpamFilter.py
+++ SpamFilter.py
@@ -1,3 +1,11 @@
+'''
+Author: Eshaan Shah
+Spam Filter using Naive Bayesian Classification, 
+Support Vector Machines and Decision Tree Learning.
+Takes as input training and testing set directory. 
+Computes the confusion matrix, and model prediction accuracy.
+'''
+
 import os
 import numpy as np
 import argparse
@@ -8,6 +16,14 @@ from sklearn.metrics import confusion_matrix
 from sklearn import metrics
 
 def predictResults(testingFeatureMatrix, testingLabels, model):
+    '''
+    Predicts the labels for test emails
+    Computes the confusion matrix using the predicted and known labels
+    param testingFeatureMatrix: Feature Matrix of the testing set 
+    param testingLabels: Labels of the testing set emails (spam or not spam)
+    param model: The model to be used
+    return: confusion matrix, model prediction accuracy 
+    '''
     result = model.predict(testingFeatureMatrix)
     confusionMatrix = confusion_matrix(testingLabels, result)
     fpr, tpr, thresholds = metrics.roc_curve(testingLabels, result, pos_label=1)
@@ -15,10 +31,25 @@ def predictResults(testingFeatureMatrix, testingLabels, model):
     return confusionMatrix, accuracy * 100
 
 def trainModel(trainingFeatureMatrix, trainingLabels, model):
+    '''
+    Trains the model based on the feature matrix and the labels
+    param trainingFeatureMatrix: Feature Matrix of the training set 
+    param trainingLabels: Labels of the training set emails (spam or not spam)
+    param model: The model to be used
+    return: trained model
+    '''
     model.fit(trainingFeatureMatrix, trainingLabels)
     return model
 
 def buildFeatureMatrix(dictionaryOfWords, path):
+    '''
+    Builds the feature matrix for training as well as testing set using the top 3000 words obtained
+    The dimensions of feature matrix are: No. of emails x 3000
+    It also generates a label matrix, containing the known email labels (spam, or not spam)
+    param dictionaryOfWOrds: dictionary containing top 3000 words
+    param path: path of the directory containing emails
+    return: list of lists Feature Matrix, list Email Labels
+    '''
     labelForEmails = list([])
     for dirPath, dirName, fileName in os.walk(path):
         for dName in dirName:
@@ -50,6 +81,13 @@ def buildFeatureMatrix(dictionaryOfWords, path):
     return featureMatrix, emailLabels
 
 def createWordDictionary(srcPath):
+    '''
+    Creates a word dictionary by sequentially processing emails from training set
+    Uses this dictionary to obtain a dictionary of top 3000 words
+    These top 3000 words will be used as features
+    param srcPath: path of the training set directory
+    return: Dictionary of top 3000 words using training set
+    '''
     wordList = list([])
     for dirPath, dirName, fileName in os.walk(srcPath):
         for dName in dirName:
@@ -72,6 +110,12 @@ def createWordDictionary(srcPath):
     return dictionaryOfWords
 
 def argumentParsing():
+    '''
+    Parses the command line arguments
+    The arguments parsed are: training set path, testing set path
+    Displays appropriate help messages
+    return: String train path, String test path
+    '''
     argParser = argparse.ArgumentParser(description="SpamFilter")
     argParser.add_argument("trainPath", type=str,
                            help="Training data path required")
@@ -83,6 +127,11 @@ def argumentParsing():
     return trainPath, testPath
 
 def main():
+    '''
+    Main method
+    Calls other functions
+    return: None
+    '''
     trainPath, testPath = argumentParsing()
 
     print("")
